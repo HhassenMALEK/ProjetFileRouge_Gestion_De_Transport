97f7b2ec633ca4044223903733edd7300eac5713
tiss <mathis.basier@gmail.com>
2025-05-12 11:04:34 +0200

    Init projet spring

---
+++ ouiMouve/src/main/java/com/api/ouimouve/OuiMouveApplication.java
@@ -0,0 +1,13 @@
+package com.api.ouimouve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuiMouveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuiMouveApplication.class, args);
+    }
+
+}
---
+++ ouiMouve/src/test/java/com/api/ouimouve/OuiMouveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ouimouve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuiMouveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
