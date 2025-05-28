7c9f210716e25a50663869406d26a5c20b03823a
Hassen MALEK <156911431+HhassenMALEK@users.noreply.github.com>
2025-05-28 10:02:29 +0200

    AJout de l'envoi de mail automatisé lors de la création, supression et modification dce covoit
    * update updateSite
    
    * update Email Module with mailtrap
    
    * update Email Module with mailtrap
    
    * update Email Module with mailtrap
    
    * update data

--- ouiMouve/src/main/resources/sql/data/02-Users.sql
+++ ouiMouve/src/main/resources/sql/data/02-Users.sql
@@ -3,4 +3,5 @@ INSERT INTO user (id, first_name, last_name, email, password, role, license_numb
 (1, 'Admin', 'Système', 'admin@ouimouve.fr', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG', 'ADMIN', 'A12345678'),
 (2, 'Marie', 'Dupont', 'marie.dupont@gmail.com', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG', 'USER', 'B98765432'),
 (3, 'Jean', 'Martin', 'jean.martin@yahoo.fr', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG', 'USER', 'C45678901'),
-(4, 'Sophie', 'Legrand', 'sophie.legrand@hotmail.com', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG', 'USER', 'D12398765');
+(4, 'Sophie', 'Legrand', 'sophie.legrand@hotmail.com', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG', 'USER', 'D12398765'),
+(5, 'Hassen', 'MALEK', 'h.hassen.malek@gmail.com', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG', 'USER', 'D1279876');
