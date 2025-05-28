63f552c6091a2bfdcbf58350d007be9d67bacf6f
Tissmax <mathis.basier@gmail.com>
2025-05-28 15:21:45 +0200

    Menu

--- ouiMouveFront/src/features/header/components/menu/menu.component.ts
+++ ouiMouveFront/src/features/header/components/menu/menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
-export class MenuComponent {
-
-}
+export class MenuComponent {}
