a359370051fcb7404352fe6e378fae3f85af4216
Tissmax <mathis.basier@gmail.com>
2025-05-28 17:59:28 +0200

    Avancement sur la barre de recherche

--- ouiMouveFront/src/features/carpooling/components/carpooling-list/carpooling-list.component.ts
+++ ouiMouveFront/src/features/carpooling/components/carpooling-list/carpooling-list.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CarpoolingCardComponent } from '../carpooling-card/carpooling-card.component';
 import { CarPoolingControllerService } from '../../../../service/api/carPoolingController.service';
 import { CarPoolingResponseDto } from '../../../../service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carpooling-list',
@@ -12,23 +13,34 @@ import { CarPoolingResponseDto } from '../../../../service';
 export class CarpoolingListComponent {
   carPoolingService = inject(CarPoolingControllerService);
   carPoolings: CarPoolingResponseDto[] = [];
+  router = inject(Router);
   ngOnInit() {
-    // Call getAll with 'body' for observe, false for reportProgress,
-    // and options specifying httpHeaderAccept as 'application/json'
-    this.carPoolingService
-      .getAll(
-        'body', // observe
-        false, // reportProgress
-        { httpHeaderAccept: 'application/json' as any } // options
-      )
-      .subscribe({
-        next: (response: CarPoolingResponseDto[]) => {
-          // Type the response
-          this.carPoolings = response;
-        },
-        error: (error) => {
-          console.error('Error fetching carpoolings:', error);
-        },
-      });
+    // Attempt to get data from router state
+    const navigation = this.router.getCurrentNavigation();
+    const stateData = navigation?.extras.state?.['data'] as
+      | CarPoolingResponseDto[]
+      | undefined;
+
+    if (stateData) {
+      console.log('Data received from state:', stateData);
+      this.carPoolings = stateData;
+    } else {
+      // Fallback to fetching all carpoolings if no state data is found
+      console.log('No state data found, fetching all carpoolings.');
+      this.carPoolingService
+        .getAll(
+          'body', // observe
+          false, // reportProgress
+          { httpHeaderAccept: 'application/json' as any } // options
+        )
+        .subscribe({
+          next: (response: CarPoolingResponseDto[]) => {
+            this.carPoolings = response;
+          },
+          error: (error) => {
+            console.error('Error fetching carpoolings:', error);
+          },
+        });
+    }
   }
 }
--- ouiMouveFront/src/features/search-bar/components/search-covoit/search-covoit.component.ts
+++ ouiMouveFront/src/features/search-bar/components/search-covoit/search-covoit.component.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { InputIconComponent } from '../../../../shared/components/input-icon/input-icon.component';
+import { CarPoolingControllerService } from '../../../../service/api/carPoolingController.service';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { CarPoolingResponseDto } from '../../../../service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-covoit',
-  imports: [InputIconComponent],
+  imports: [InputIconComponent, ButtonComponent],
   templateUrl: './search-covoit.component.html',
   styleUrl: './search-covoit.component.scss',
 })
 export class SearchCovoitComponent {
-  departure = '';
-  arrival = '';
-  date = '';
-  passanger = 1;
+  carPoolingService = inject(CarPoolingControllerService);
+  searchParameters = {
+    siteDeparture: '',
+    siteDestination: '',
+    startDate: '',
+    passanger: 1,
+  };
+  convoitResults: CarPoolingResponseDto[] | undefined = undefined;
+  router = inject(Router);
+  searchCovoit() {
+    this.carPoolingService
+      .filterByCriteria(
+        undefined,
+        undefined,
+        this.searchParameters.startDate || undefined,
+        this.searchParameters.siteDeparture
+          ? { name: this.searchParameters.siteDeparture }
+          : undefined,
+        this.searchParameters.siteDestination
+          ? { name: this.searchParameters.siteDestination }
+          : undefined,
+        undefined,
+        'body',
+        false,
+        { httpHeaderAccept: 'application/json' as any }
+      )
+      .subscribe({
+        next: (response) => {
+          this.convoitResults = response;
+          this.router.navigate(['/search/covoit'], {
+            state: { data: this.convoitResults },
+          });
+        },
+        error: (error) => {
+          console.error('Error searching for carpooling:', error);
+          console.log('Search parameters:', this.searchParameters);
+        },
+      });
+  }
 }
