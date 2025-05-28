df9f37e131e06255c5905c8c08bc3725f9413ecd
Tissmax <mathis.basier@gmail.com>
2025-05-28 11:20:53 +0200

    Ajout du composant input icon

--- ouiMouveFront/src/features/carpooling/components/carpooling-list/carpooling-list.component.ts
+++ ouiMouveFront/src/features/carpooling/components/carpooling-list/carpooling-list.component.ts
@@ -1,33 +1,35 @@
 import { Component, inject } from '@angular/core';
-import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { CarpoolingCardComponent } from '../carpooling-card/carpooling-card.component';
 import { CarPoolingControllerService } from '../../../../api/api/carPoolingController.service';
 import { CarPoolingResponseDto } from '../../../../api';
+import { SearchBarComponent } from '../../../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-carpooling-list',
   imports: [CarpoolingCardComponent, SearchBarComponent],
   templateUrl: './carpooling-list.component.html',
-  styleUrl: './carpooling-list.component.scss'
+  styleUrl: './carpooling-list.component.scss',
 })
 export class CarpoolingListComponent {
   carPoolingService = inject(CarPoolingControllerService);
   carPoolings: CarPoolingResponseDto[] = [];
   ngOnInit() {
-    // Call getAll with 'body' for observe, false for reportProgress, 
+    // Call getAll with 'body' for observe, false for reportProgress,
     // and options specifying httpHeaderAccept as 'application/json'
-    this.carPoolingService.getAll(
-      'body', // observe
-      false,  // reportProgress
-      { httpHeaderAccept: 'application/json'  as any} // options
-    ).subscribe({
-      next: (response: CarPoolingResponseDto[]) => { // Type the response
-        this.carPoolings = response;
-      },
-      error: (error) => {
-        console.error('Error fetching carpoolings:', error);
-      }
-    });
+    this.carPoolingService
+      .getAll(
+        'body', // observe
+        false, // reportProgress
+        { httpHeaderAccept: 'application/json' as any } // options
+      )
+      .subscribe({
+        next: (response: CarPoolingResponseDto[]) => {
+          // Type the response
+          this.carPoolings = response;
+        },
+        error: (error) => {
+          console.error('Error fetching carpoolings:', error);
+        },
+      });
   }
-
 }
