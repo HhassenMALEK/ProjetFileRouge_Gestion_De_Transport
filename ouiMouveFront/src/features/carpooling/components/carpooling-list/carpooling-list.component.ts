09eb9a754807efe01e2cb3ae16f5166f2082f8ba
Tissmax <mathis.basier@gmail.com>
2025-06-06 12:31:11 +0200

    Réservation fonctionelle

--- ouiMouveFront/src/features/carpooling/components/carpooling-list/carpooling-list.component.ts
+++ ouiMouveFront/src/features/carpooling/components/carpooling-list/carpooling-list.component.ts
@@ -2,6 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CarPoolingResponseDto } from '@openapi/model/carPoolingResponseDto';
 import { CarPoolingFilteringService } from '@shared/service/carpooling-filtering.service';
 import { CarpoolingCardComponent } from '@shared/components/carpooling-card/carpooling-card.component';
+import { AuthService } from '@shared/service/auth.service';
+import {
+  CarPoolingReservationsControllerService,
+  CarPoolingReservationsCreateDTO,
+} from '@openapi/index';
 @Component({
   selector: 'app-carpooling-list',
   imports: [CarpoolingCardComponent],
@@ -11,6 +16,8 @@ import { CarpoolingCardComponent } from '@shared/components/carpooling-card/carp
 export class CarpoolingListComponent implements OnInit {
   carPoolings: CarPoolingResponseDto[] = [];
   carPoolingFilteringService = inject(CarPoolingFilteringService);
+  auth = inject(AuthService);
+  reservationService = inject(CarPoolingReservationsControllerService);
   ngOnInit(): void {
     // Fetch carpoolings when the component initializes
 
@@ -22,8 +29,19 @@ export class CarpoolingListComponent implements OnInit {
       }
     );
   }
-  handleReservation(): void {
-    // Handle reservation logic here
-    console.log('Reservation button clicked');
+  handleReservation(carpoolingId: number): void {
+    const dto: CarPoolingReservationsCreateDTO = {
+      carPoolingId: carpoolingId,
+    };
+    this.reservationService.createCarPoolingReservation(dto).subscribe({
+      next: (response) => {
+        console.log('Reservation created successfully:', response);
+        // Optionally, you can refresh the carpooling list or show a success message
+      },
+      error: (error) => {
+        console.error('Error creating reservation:', error);
+        // Handle error appropriately, e.g., show an error message to the user
+      },
+    });
   }
 }
