562af911e6741fd0068895054cd9e6fc3790998f
DaudevInfo <sebastien.daudey@gmail.com>
2025-06-06 12:01:32 +0200

    Mise au point des cards ServiceVéhicle

--- ouiMouveFront/src/features/ServiceVehicle/components/service-vehicle-list/service-vehicle-list.component.ts
+++ ouiMouveFront/src/features/ServiceVehicle/components/service-vehicle-list/service-vehicle-list.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ServiceVehicleDto } from '@openapi/model/serviceVehicleDto';
+import { ServiceVehicleFilteringService } from '@shared/service/serviceVehicle-filtering.service';
+import { ServiceVehicleItemComponent } from '../service-vehicle-item/service-vehicle-item.component';
 
 @Component({
   selector: 'app-service-vehicle-list',
-  imports: [],
+  imports: [ServiceVehicleItemComponent],
   templateUrl: './service-vehicle-list.component.html',
   styleUrl: './service-vehicle-list.component.scss'
 })
 export class ServiceVehicleListComponent {
+
+  serviceVehicles: ServiceVehicleDto[] = [];
+  color : string = '';
+  serviceVehicleFilteringService = inject(ServiceVehicleFilteringService);
+
+  ngOnInit(): void {
+    // Fetch service vehicles when the component initializes
+
+    // Subscribe to service vehicle updates from the filtering service
+    this.serviceVehicleFilteringService.currentServiceVehicles.subscribe(
+      (serviceVehicles: ServiceVehicleDto[]) => {
+        console.log('Received service vehicle updates:', serviceVehicles);
+        this.serviceVehicles = serviceVehicles;
+      }
+    );
+  }
 
 }
